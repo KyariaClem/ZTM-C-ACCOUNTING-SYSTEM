1804206816b21850b059a65f2af033f51a84a5c1
kyariaclemons <kyarialove@gmail.com>
2024-04-01 20:57:08 -0500

    Added greeting in systema and set up syntax for checking and preium accounts

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// The following Features Should Be Implemented:
+
+// Greet the user with a welcome message. Something like "Welcome to the Accounting System 1.0."
+Console.WriteLine("Welcome to the Accounting System 1.0");
+// The accounting system should have a checking account and a premium account.
+class CheckingAccount {
+
+}
+
+class PremiumAccount {
+
+}
+// A checking account has an ID (number) and a balance (decimal number).
+
+// You should be able to set a starting balance when creating a checking account.
+
+// You should be able to deposit money (decimal number) at any time after creating a checking account.
+
+// You should be able to calculate interest based on the current balance of the checking account and an input.
+
+// You should be able to print the ID and the current balance of a checking account at any time.
+
+// The premium account has the same behavior as the checking account. The only difference is that the premium account gets a 1% extra interest on top of the user input.
+
+// You should be able to transfer money from one account to another at any time.
+
+// The program should execute the following operations in order:
+
+// Print the welcome message.
+
+// Create a checking account with an ID of 100 and starting balance of 0.
+
+// Create a premium account with an ID of 200 and starting balance of 0.
+
+// Print the balance of both accounts.
+
+// Deposit 2000 to the checking account.
+
+// Deposit 6000 to the premium account.
+
+// Print the balance of both accounts.
+
+// Transfer 2000 from the premium account to the checking account.
+
+// Print the balance of both accounts.
+
+// Calculate the interest for both accounts based on a 3% interest rate.
+
+// Print the interest for both accounts.
+
+// Print the balance of both accounts after adding the interest to the accounts.
